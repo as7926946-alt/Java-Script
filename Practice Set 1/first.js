90b8e17a67a17b60a3a5c0c098df8ea22454f86b
as7926946-alt <as7926946@gmail.com>
2025-12-19 22:42:04 +0530

    BAsic Variables

---
+++ Practice Set 1/first.js
@@ -0,0 +1,31 @@
+// code for creating a variable of string type and adding a number
+/*let a="Anshika";
+let b=6;
+console.log(a+b);*/
+
+// code for finding the data type 
+/*let a="Anshika";
+let b=6;
+console.log(typeof (a+b));*/
+
+// code for adding a new key to the const object 
+/*const a ={
+    name:"Anshika",
+    section:1,
+    isPrinciple:false
+}
+a['friend']="Saanvi"
+console.log(a);*/
+
+// code for creating a word meaning dictionary 
+/*const dic={
+    Ephemeral: "Lasting for a very short time",
+   Ubiquitous: "Present, appearing, or found everywhere",
+Mellifluous: "sweet or musical; pleasant to hear",
+Ineffable: "Too great or extreme to be expressed or described in words",
+Serendipity: "The occurrence and development of events by chance in a happy or beneficial way"
+}
+console.log(dic.Ineffable);
+console.log(dic['Serendipity']);*/
+
+
