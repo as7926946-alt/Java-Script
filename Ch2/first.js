0ff94ca53a5d401cb24b575b05ea677f1618c94a
as7926946-alt <as7926946@gmail.com>
2025-12-21 20:51:29 +0530

    Conditional statement and Switch case

---
+++ Ch2/first.js
@@ -0,0 +1,63 @@
+// code for checking whether the age is between 10 and 20 using ternary operator 
+/*let a=prompt("Enter age of the person");
+console.log(a>=10 && a<=20?"Age lies between 10 and 20" : "Age does not lie between 10 and 20");*/
+
+// code for checking whether the age is between 10 and 20 using if else
+/*let a=prompt("Enter the age of the person");
+if(a>=10 && a<=20){
+    console.log("Age lies between 10 and 20");
+}
+else{
+    console.log("Age does not lie between 10 and 20");
+}*/
+
+// code for printing weekdays and weekends using switch case
+/*let choice=prompt("Enter day");
+switch(choice){
+    case 'Monday':
+    case 'Tuesday':
+    case 'Wednesday':
+    case 'Thursday':
+    case 'Friday':
+    case 'monday':
+    case 'tuesday':
+    case 'wednesday':
+    case 'thursday':
+    case 'friday':
+        console.log("Weekday");
+        break;
+    case 'Saturday':
+    case 'Sunday':
+    case 'saturday':
+    case 'sunday':
+        console.log("Weekend");
+        break;
+    default:
+        console.log("Invalid day");
+}*/
+
+// code for checking whether number is divisible by 2 and 3 
+/*let a=prompt("Enter number");
+if(a%2==0 && a%3==0){
+    console.log("Number is divisible by both 2 and 3");
+}
+else{
+    console.log("Number is not divisible by both 2 and 3");
+}*/
+
+// code for checking whether number is divisible by 2 or 3 
+/*let a=prompt("Enter number");
+if(a%2==0 || a%3==0){
+    console.log("Number is divisible");
+}
+else{
+    console.log("Number is not divisible");
+}*/
+
+// code for eligibility to drive using ternary operator 
+/*let a=prompt("Enter your age");
+a=Number.parseInt(a);
+console.log((a<=18?"Can not drive":"Can drive"));*/
+
+
+
