5c123c87d3034d278855629500ce7196c1bcc2de
as7926946-alt <as7926946@gmail.com>
2025-12-22 21:19:04 +0530

    Loops and function

---
+++ Ch3/first.js
@@ -0,0 +1,36 @@
+// code for printing marks of a student using for loop
+/*let marks={
+    harry:98,
+    rohan:70,
+    monika:3
+}
+for(let i=0;i<Object.keys(marks).length;i++){
+    console.log(Object.keys(marks)[i]+marks[Object.keys(marks)[i]])
+}*/
+
+//code for printing marks of a student using for in loop 
+/*let obj={
+    harry:98,
+    rohan:70,
+}
+for(let a in obj){
+    console.log(a+obj[a]);
+}*/
+
+// code for taking input until user enters the correct number
+/*let cn=45;
+let  i;
+while(i!=cn){
+ i=prompt("Enter number");
+ if(i!=cn){
+    console.log("Try again");
+ }
+}
+console.log("You have entered correct number");*/
+
+// code for finding mean of a five numbers using functions
+/*const mean=(a,b,c,d)=>{
+    return (a+b+c+d)/4;
+}
+console.log(mean(4,5,6,7));*/
+
