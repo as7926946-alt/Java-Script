6d7b1d0a469b80a514b090aa0c11e3c4e8afd9ba
as7926946-alt <as7926946@gmail.com>
2026-01-18 13:37:24 +0530

    Array and its methods

---
+++ CH5/first.js
@@ -0,0 +1,44 @@
+//create an array and input number from user
+// let arr=[];
+// let count=Number(prompt("How many numbers do you want to add??"));
+// for (let i = 0; i < count; i++) {
+//     let num = Number(prompt("Enter number " + (i + 1) + ":"));
+//     arr.push(num);
+// }
+// console.log(arr);
+
+//keep adding number until user enters 0
+// let count=Number(prompt("Enter number (0 to stop)"));
+// let sum=0;
+// while(count!=0){
+//     sum=sum+count;
+//      count=Number(prompt("Enter number (0 to stop)"))
+// }
+// console.log(sum); 
+
+//Filter numbers divisible by 10
+// let arr=[];
+// let count=Number(prompt("How many number do you want to add in your array?"));
+// for(let i=0;i<count;i++){
+//     let num=Number(prompt((i+1)));
+//     arr.push(num);
+// }
+// let newarr=arr.filter((a)=>{
+//       return a%10==0;
+// });
+// console.log(newarr);
+
+// Create an array of square of given numbers
+// let arr=[2,5,3,6,8];
+// let a=arr.map((value)=>{
+//     return value*value;
+// });
+// console.log(a);
+
+//Calculating factorial using reduce 
+// let n=5;
+// let arr=[1,2,3,4,5];
+// let newarr=arr.reduce((h1,h2)=>{
+//     return h1*h2;
+// });
+// console.log(newarr);
